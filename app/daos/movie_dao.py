a1bbd5106374b1129db585bf733771c15af498be
peres <tiago.sousa@viz.ai>
2022-05-15 11:55:42 +0100

    Update insert movies endpoint to not allow insertion of movies that already exist in the db

--- app/daos/movie_dao.py
+++ app/daos/movie_dao.py
@@ -32,7 +32,16 @@ class MovieDAO(metaclass=Singleton):
     def insert_many(self, movies: List[Movie]):
         with MongoClient('mongodb://127.0.0.1:27017') as client:
             movie_collection = self._get_collection(client)
-            movies_to_insert = [movie.dict() for movie in movies]
+            movies_to_insert = []
+            duplicated_movie_names = []
+            for movie in movies:
+                if movie_collection.count_documents({"name": movie.dict()["name"]}) > 0:
+                    duplicated_movie_names.append(movie.dict()["name"])
+                else:
+                    movies_to_insert.append(movie.dict())
+            if len(duplicated_movie_names) > 0:
+                raise HTTPException(status_code=400, detail="The following movies already exist in the db: " +
+                                                            ', '.join(duplicated_movie_names))
             movie_collection.insert_many(movies_to_insert)
         return movies
 

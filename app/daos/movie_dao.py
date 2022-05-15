98eda9e668915d9c79e4d7425f008b91de4c7a7f
Tiago Peres de Sousa <peres@cy219lyv9n.lan>
2022-05-15 11:00:31 +0100

    Remove dtos folder. Including models in the service and dao layers

--- app/daos/movie_dao.py
+++ app/daos/movie_dao.py
@@ -1,5 +1,8 @@
+from typing import List
 from pymongo import MongoClient
 from fastapi import HTTPException
+
+from app.models.movie_model import Movie, MovieDetailsUpdate
 
 
 class Singleton(type):
@@ -26,7 +29,7 @@ class MovieDAO(metaclass=Singleton):
     def _get_collection(self, client):
         return client[self.db][self.collection]
 
-    def insert_many(self, movies):
+    def insert_many(self, movies: List[Movie]):
         with MongoClient('mongodb://127.0.0.1:27017') as client:
             movie_collection = self._get_collection(client)
             movies_to_insert = [movie.dict() for movie in movies]
@@ -47,14 +50,14 @@ class MovieDAO(metaclass=Singleton):
             movie_collection = self._get_collection(client)
             movie_collection.drop()
 
-    def delete_one(self, movie_name):
+    def delete_one(self, movie_name: str):
         with MongoClient('mongodb://127.0.0.1:27017') as client:
             movie_collection = self._get_collection(client)
             if movie_collection.count_documents({"name": movie_name}) == 0:
                 raise HTTPException(status_code=404, detail="Movie not found")
             movie_collection.delete_one({"name": movie_name})
 
-    def update_one(self, movie_name, details):
+    def update_one(self, movie_name: str, details: MovieDetailsUpdate):
         with MongoClient('mongodb://127.0.0.1:27017') as client:
             movie_collection = self._get_collection(client)
             if movie_collection.count_documents({"name": movie_name}) == 0:
--- app/services/movie_service.py
+++ app/services/movie_service.py
@@ -1,23 +1,26 @@
-from app.dtos.movie_dto import MovieDTO
+from typing import List
+
+from app.daos.movie_dao import MovieDAO
+from app.models.movie_model import Movie, MovieDetailsUpdate
 
 
 class MovieService:
     @staticmethod
-    def save(movies):
-        MovieDTO.insert_many(movies)
+    def save(movies: List[Movie]):
+        MovieDAO().insert_many(movies)
 
     @staticmethod
     def get_all():
-        return MovieDTO.find_all()
+        return MovieDAO().find_all()
 
     @staticmethod
     def delete_all():
-        MovieDTO.delete_all()
+        MovieDAO().delete_all()
 
     @staticmethod
-    def delete_one(movie_name):
-        MovieDTO.delete_one(movie_name)
+    def delete_one(movie_name: str):
+        MovieDAO().delete_one(movie_name)
 
     @staticmethod
-    def update_one(movie_name, details):
-        MovieDTO.update_one(movie_name, details)
+    def update_one(movie_name: str, details: MovieDetailsUpdate):
+        MovieDAO().update_one(movie_name, details)

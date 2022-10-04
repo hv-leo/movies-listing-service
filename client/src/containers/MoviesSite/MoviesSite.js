3692ef4041133615bccbde8d8907d10285791a6c
peres <tiago.sousa@viz.ai>
2022-10-05 00:03:55 +0100

    First step of implementation of auth0 on the frontend side

--- client/src/containers/MoviesSite/MoviesSite.js
+++ client/src/containers/MoviesSite/MoviesSite.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Route } from 'react-router-dom';
-import Cookies from 'universal-cookie';
+import { useAuth0 } from "@auth0/auth0-react";
+import {
+  NavItem,
+  Button,
+    NavbarBrand,
+    NavbarToggler
+
+} from "reactstrap";
 
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
@@ -26,11 +33,55 @@ genres = genresList.map( genre => {
     );
 } );
 
-const MoviesSite = (props) => (
-    <Container>
+const MoviesSite = (props) => {
+    const {
+    isAuthenticated,
+    loginWithRedirect,
+    logout,
+        } = useAuth0();
+
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
+    const logoutWithRedirect = () =>
+    logout({
+      returnTo: window.location.origin,
+    });
+    return (
+        <div className="nav-container">
+        <Container>
         <Navbar bg="light" variant="light" expand="lg">
+            <NavbarBrand className="logo" />
+            <NavbarToggler onClick={toggle} />
+            <Nav className="d-none d-md-block" navbar>
+                {!isAuthenticated && (
+                    <NavItem>
+                        <Button
+                            id="qsLoginBtn"
+                            color="primary"
+                            className="btn-margin"
+                            style={{ marginLeft: "auto" }}
+                            onClick={() => loginWithRedirect()}
+                        >
+                            Log in
+                        </Button>
+                    </NavItem>
+                )}
+                {isAuthenticated && (<NavItem>
+                        <Button
+                            id="qsLoginBtn"
+                            color="primary"
+                            className="btn-margin"
+                            style={{ marginLeft: "auto" }}
+                            onClick={() => logoutWithRedirect()}
+                        >
+                            Log out
+                        </Button>
+                    </NavItem>)
+                }
+            </Nav>
             <Navbar.Brand href="/movies-feed/">MoviesFeed</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     {genres}
@@ -39,6 +90,7 @@ const MoviesSite = (props) => (
         </Navbar>
         <Route path={"/:genre"} component={MoviesFeed}/>
     </Container>
-);
+        </div>)
+};
 
 export default MoviesSite;
--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Auth0Provider } from "@auth0/auth0-react";
+import history from "./utils/history";
+import { getConfig } from "./config";
 
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const onRedirectCallback = (appState) => {
+  history.push(
+    appState && appState.returnTo ? appState.returnTo : window.location.pathname
+  );
+};
+
+const config = getConfig();
+
+const providerConfig = {
+  domain: config.domain,
+  clientId: config.clientId,
+  ...(config.audience ? { audience: config.audience } : null),
+  redirectUri: window.location.origin,
+  useRefreshTokens: false,
+  onRedirectCallback,
+};
+
 ReactDOM.render(
   <React.StrictMode>
+    <Auth0Provider {...providerConfig}>
     <App />
+      </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
